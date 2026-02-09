c16835e1e3304e8067380801a8e178a22b4892f2
SahasraYanumula21 <sahasrayanumula06@gmail.com>
2026-02-09 11:16:06 +0530

    Final Vercel simplification: direct Express export for index function

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,2 @@
 import app from '../server/server.ts';
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default (req: VercelRequest, res: VercelResponse) => {
-    return app(req, res);
-};
+export default app;
